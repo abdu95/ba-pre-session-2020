05d0e9fb8113c1bbba8ab0ad652ad64b69ab6ff5
abdumalik2014 <abdumalik2014@gmail.com>
2020-09-27 13:39:49 +0200

    fix: script that lists files of lName_fName dir

--- exam/malikov_abduvosid/IT/my_script.sh
+++ exam/malikov_abduvosid/IT/my_script.sh
@@ -2,6 +2,6 @@
 cd exam/malikov_abduvosid/IT
 mkdir made_by_my_script
 echo "Hello World" > made_by_my_script/hello.txt
-ls .. > made_by_my_script/list_of_files.txt
+ls made_by_my_script > made_by_my_script/list_of_files.txt
 
 
